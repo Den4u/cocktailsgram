e8307160d44ac7ca18352285d9a61bc69af4169f
Денис Бердников <berdnikov4uu@gmail.com>
2024-03-24 07:31:52 +0300

    email_use_ssl

--- backend/cocktailgram/cocktailgram/settings.py
+++ backend/cocktailgram/cocktailgram/settings.py
@@ -123,7 +123,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 EMAIL_BACKEND = os.getenv('EMAIL_BACKEND')
 EMAIL_HOST = os.getenv('EMAIL_HOST')
 EMAIL_PORT = os.getenv('EMAIL_PORT')
-EMAIL_USE_SSL = os.getenv('EMAIL_USE_SSL', 'False').lower() == 'true'
+EMAIL_USE_SSL = True
 
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
